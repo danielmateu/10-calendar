f8755baad35a2194d2a51507f62bca7602cd0c0a
Daniel Mateu <danielmateu86@gmail.com>
2022-08-18 19:03:46 +0200

    useForm con validaciones añadido

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,7 +6,7 @@ import { CalendarPage } from "../calendar";
 
 export const AppRouter = () => {
 
-  const authStatus = 'authenticated';
+  const authStatus = 'not-authenticated'; // 'authenticated'; // 'not-authenticated'
 
   
 
