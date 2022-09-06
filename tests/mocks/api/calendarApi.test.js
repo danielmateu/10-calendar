0bb438f98612bc8ee93e1cf27f1d69b7b015987d
Daniel Mateu <danielmateu86@gmail.com>
2022-09-06 15:26:35 +0200

    Pruebas en calendarApi

--- tests/mocks/api/calendarApi.test.js
+++ tests/mocks/api/calendarApi.test.js
@@ -1,9 +1,19 @@
 import calendarApi from "../../../src/api/calendarApi"
 
 describe('Pruebas sobre calendarApi', () => { 
+
     test('Debe de tener la configuración por defecto', () => {  
         // console.log(calendarApi);
         console.log(process.env);
         expect(calendarApi.defaults.baseURL).toBe(process.env.VITE_API_URL);
+    })
+
+    test('Debe de tener el x-token en el header de todas las peticiones', async() => { 
+
+        const token = 'ABC-123-XYZ'
+        localStorage.setItem('token', 'ABC-123-XYZ');
+        const res = await calendarApi.get('/auth');
+
+        expect(res.config.headers['x-token']).toBe(token);
     })
 })
